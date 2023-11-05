ced7332dbe88bedfc52a4fbbb1123de076c317ed
ZHKO1 <zhko11993@gmail.com>
2023-11-05 14:50:53 +0800

    新增riscv32指令，能跑通am-kernels/tests/cpu-tests/大部分测试例子，除了string和hello-str

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -37,6 +37,7 @@
 
 typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
 typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;
+typedef MUXDEF(CONFIG_ISA64, __int128_t, int64_t)  dsword_t;
 #define FMT_WORD MUXDEF(CONFIG_ISA64, "0x%016" PRIx64, "0x%08" PRIx32)
 
 typedef word_t vaddr_t;
