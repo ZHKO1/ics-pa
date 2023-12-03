2aa2def509943744b9401fd6214fe30533010597
ZHKO1 <zhko11993@gmail.com>
2023-12-03 08:54:04 +0000

    实现DiffTest的isa_difftest_checkregs函数

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,32 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+static void isa_difftest_checkregs_log(const char *reg_name, word_t dut_value, word_t ref_value, bool *is_diff) {
+  if(!*is_diff) {
+    printf("Difftest:\n");
+  }
+  printf("%-15s" ANSI_FMT(FMT_REG_DISPLAY, ANSI_FG_RED) ANSI_FMT(FMT_REG_DISPLAY, ANSI_FG_GREEN) "\n", reg_name, dut_value, ref_value);
+  *is_diff = true;
+}
+
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  size_t reg_len = MUXDEF(CONFIG_RVE, 16, 32);
+  bool is_diff = false;
+  for(size_t i = 0; i < reg_len; i++){
+    word_t dut_reg_value = cpu.gpr[i];
+    word_t ref_reg_value = ref_r->gpr[i];
+    if(dut_reg_value != ref_reg_value){
+      isa_difftest_checkregs_log(reg_name(i), dut_reg_value, ref_reg_value, &is_diff);
+    }
+  }
+  vaddr_t dut_pc = cpu.pc;
+  vaddr_t ref_pc = ref_r->pc;
+  if(dut_pc != ref_pc){
+    isa_difftest_checkregs_log("$pc", dut_pc, ref_pc, &is_diff);
+  }
+
+  return !is_diff;
 }
 
 void isa_difftest_attach() {
