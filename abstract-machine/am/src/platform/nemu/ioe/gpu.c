f27b1fe553e88653e6c43fad16d9e62a42ab6262
ZHKO1 <zhko11993@gmail.com>
2023-12-12 20:44:35 +0800

    支持AM_GPU_CONFIG 和 AM_GPU_FBDRAW

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,19 +3,39 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static int gpu_width = 0;
+static int gpu_height = 0;
+
 void __am_gpu_init() {
+  gpu_width = io_read(AM_GPU_CONFIG).width;
+  gpu_height = io_read(AM_GPU_CONFIG).height;
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t result = inl(VGACTL_ADDR);
+  int width = result >> 16;
+  int height = result & 0xFFFF;
+  int vmemsz = width * height * sizeof(uint32_t);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = width, .height = height,
+    .vmemsz = vmemsz
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+  int x = ctl->x, y = ctl->y;
+  void *pixels = ctl->pixels;
+  int w = ctl->w, h = ctl->h;
+  for(int i = 0; i < w; i++) {
+    for(int j = 0; j < h; j++) {
+      uint32_t *pixel = (uint32_t *)(uintptr_t)pixels + i + j * w;
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR + (i + x) + (j + y) * gpu_width;
+      outl((uintptr_t)fb, *pixel);
+    }
+  }
+  bool sync = ctl->sync;
+  if (sync) {
     outl(SYNC_ADDR, 1);
   }
 }
