73c23d9552cdac6cbda940a16fd69b6e85ffd756
ZHKO1 <zhko11993@gmail.com>
2023-12-12 06:26:29 +0000

    支持AM_INPUT_KEYBRD

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,9 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t am_scancode = inl(KBD_ADDR);
+  bool keydown = (am_scancode & KEYDOWN_MASK) ? true : false;
+  int keycode = am_scancode & 0xFF;
+  kbd->keydown = keydown;
+  kbd->keycode = keycode;
 }
