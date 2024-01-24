c647b8fc3142252a493f58af0de19f6a8ee60b81
ZHKO1 <zhko11993@gmail.com>
2024-01-24 22:05:25 +0800

    支持SDL_LoadWAV和SDL_MixAudio（有待实机上验证）

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <stdlib.h>
 
+static void SDL_LoadWAV_read(void *buf, size_t offset, size_t len, FILE *file);
+
 static SDL_AudioSpec config = {};
 static bool audio_init = false;
 static bool is_pause = false;
@@ -73,16 +75,100 @@ void SDL_PauseAudio(int pause_on) {
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
-  // assert(0);
+  // TODO 这里有个问题就是SDL_MixAudio是怎么判断format的？按理说8位和16位的混音方式应该有差异？
+  uint32_t size = len / sizeof(int16_t);
+  for (size_t i = 0; i < size; i++) {
+    int16_t src_val = *((int16_t *)src + i);
+    int16_t dst_val = *((int16_t *)dst + i);
+    int32_t val = (int32_t)dst_val + (int32_t)src_val;
+    int16_t result = 0;
+    if(val > INT16_MAX) {
+      result = INT16_MAX;
+    } else if(val < INT16_MIN) {
+      result = INT16_MIN;
+    } else {
+      result = (int16_t)val;
+    }
+    *((int16_t *)dst + i) = result * volume / SDL_MIX_MAXVOLUME;
+  }
 }
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
   // assert(0);
-  return NULL;
+  FILE *wave_file = fopen(file, "r");
+  assert(wave_file);
+
+  uint32_t ChunkID = 0;
+  SDL_LoadWAV_read(&ChunkID, 0, 4, wave_file);
+  assert(ChunkID == 0x46464952);
+  
+  uint32_t Format = 0;
+  SDL_LoadWAV_read(&Format, 8, 4, wave_file);
+  assert(Format == 0x45564157);
+  
+  uint32_t Subchunk1ID = 0;
+  SDL_LoadWAV_read(&Subchunk1ID, 12, 4, wave_file);
+  assert(Subchunk1ID == 0x20746D66);
+
+  uint32_t Subchunk1Size = 0;
+  SDL_LoadWAV_read(&Subchunk1Size, 16, 4, wave_file);
+
+  uint16_t AudioFormat = 0;
+  SDL_LoadWAV_read(&AudioFormat, 20, 2, wave_file);
+  assert(AudioFormat == 1); // 如果不是1，就是压缩的PCM，暂不支持
+
+  uint16_t NumChannels = 0;
+  SDL_LoadWAV_read(&NumChannels, 22, 2, wave_file);
+
+  uint32_t SampleRate = 0;
+  SDL_LoadWAV_read(&SampleRate, 24, 4, wave_file);
+
+  uint32_t ByteRate = 0;
+  SDL_LoadWAV_read(&ByteRate, 28, 4, wave_file);
+
+  uint16_t BitsPerSample = 0;
+  SDL_LoadWAV_read(&BitsPerSample, 34, 2, wave_file);
+  assert(BitsPerSample == 16); // 目前应该是只支持AUDIO_S16SYS
+
+  size_t Subchunk2ID_offset = 16 + 4 + Subchunk1Size;
+  uint32_t Subchunk2ID = 0;
+  SDL_LoadWAV_read(&Subchunk2ID, Subchunk2ID_offset, 4, wave_file);
+  assert(Subchunk2ID == 0x61746164);
+
+  size_t Subchunk2Size_offset = Subchunk2ID_offset + 4;
+  uint32_t Subchunk2Size = 0;
+  SDL_LoadWAV_read(&Subchunk2Size, Subchunk2Size_offset, 4, wave_file);
+
+  if (audio_buf) {
+    size_t data_offset = Subchunk2Size_offset + 4;
+    uint8_t *buf = malloc(Subchunk2Size);
+    assert(buf);
+    SDL_LoadWAV_read(buf, data_offset, Subchunk2Size, wave_file);
+    *audio_buf = buf;
+  }
+  if (audio_len) {
+    *audio_len = Subchunk2Size;
+  }
+  fclose(wave_file);
+  if (spec) {
+    spec->freq = SampleRate;
+    spec->format = AUDIO_S16SYS;
+    spec->channels = NumChannels;
+    spec->samples = Subchunk2Size / (NumChannels * BitsPerSample / 8);
+    return spec;
+  } else {
+    return NULL;
+  }
+}
+
+static void SDL_LoadWAV_read(void *buf, size_t offset, size_t len, FILE *file) {
+  fseek(file, offset, SEEK_SET);
+  int ret = fread(buf, len, 1, file);
+  assert(ret == 1);
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
-  // assert(0);
+  free(audio_buf);
 }
 
 void SDL_LockAudio() {
