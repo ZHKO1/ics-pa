5257e511b4d07264ce365332be37f4a2506b5515
ZHKO1 <zhko11993@gmail.com>
2024-02-19 15:54:01 +0800

    在分页机制上让Nanos-lite运行仙剑奇侠传和内核线程hello

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,8 +22,7 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  // 测试hello_fun专用
-  // context_kload(&pcb[0], hello_fun, (void *)0x1111);
+  context_kload(&pcb[0], hello_fun, (void *)0x1111);
 
   /*
   char *agv[] = {
@@ -39,7 +38,7 @@ void init_proc() {
     user_program_path,
     NULL
   };
-  context_uload(&pcb[0], user_program_path, argv, NULL);
+  context_uload(&pcb[1], user_program_path, argv, NULL);
   switch_boot_pcb();
   Log("Initializing processes...");
 
@@ -49,8 +48,7 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
   current->cp = prev;
-  // pcb_index = (current == &pcb[0] ? 1 : 0);
-  pcb_index = 0;
+  pcb_index = (current == &pcb[0] ? 1 : 0);
   current = &pcb[pcb_index];
   return current->cp;
 }
